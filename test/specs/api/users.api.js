433ecddf9faeb8fde4b7dec9c75874a141f8dbb4
Bruno Fioritto <bruno.fioritto@jam3.com>
2023-06-21 19:02:44 -0300

    Initial commit

--- test/specs/api/users.api.js
+++ test/specs/api/users.api.js
@@ -1,10 +1,19 @@
 const axios = require('axios');
 const assert = require('assert');
+const { expect } = require('chai');
 
 describe('Users API Test', () => {
 
+    it('should return a 200 response for GET request', async () => {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        expect(response.status).to.equal(200);
+    });
+
     it('should retrieve users and verify response data', async () => {
       const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+
+      console.log(response.data);
+
       const users = response.data;
   
       assert.strictEqual(users.length, 10, 'Number of users should be 10');
